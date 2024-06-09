c41bd127c1fc5187d8e1f3e2c73cd7e44dbb8c6a
shaharTasa <136985326+shaharTasa@users.noreply.github.com>
2024-06-09 17:43:38 +0300

    first files

---
+++ json_analysis/interactions_analysis/find_intreactions.py
@@ -0,0 +1,6 @@
+import json
+from json_analysis.get_alphafold_output import read_json_file
+
+full_data_json_file = read_json_file('C:\\Users\\shach\\Documents\\Alpha_interactions\\json_analysis\\json_output\\fold_Irrc75b_femnb1\\fold_lrrc75b_femnb1_full_data_0.json')
+job_request = read_json_file('C:\\Users\\shach\\Documents\\Alpha_interactions\\json_analysis\\json_output\\fold_Irrc75b_femnb1\\fold_lrrc75b_femnb1_job_request.json')
+
