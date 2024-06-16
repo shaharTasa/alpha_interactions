19387488560bd9e683efcd2b813e1c0f69f3b5f9
shaharTasa <136985326+shaharTasa@users.noreply.github.com>
2024-06-16 15:37:28 +0300

    changing the code

--- draws/header.py
+++ draws/header.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 def draw_header():
-    col1,col2=st.columns([15,100])
+    col1,col2=st.columns([20,100])
     col2.title("_This_ is **Alpha fold interactions detector** ")
-    col1.image('./assets/Picture1.png')
+    col1.image('./assets/protein.png')
