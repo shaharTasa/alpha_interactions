8b5326ef5bdc9a6c65f7b6f3f2d2f8901b7ddbc1
shaharTasa <136985326+shaharTasa@users.noreply.github.com>
2024-06-16 15:15:45 +0300

    ordering the file

--- draws/output_protein_data.py
+++ draws/output_protein_data.py
@@ -1,15 +1,10 @@
-import os
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-import streamlit as st
-import pandas as pd
-import json
-from data_analysis.json_process.utils.extrect_data import extract_data_from_full_data_json,extract_data_from_summary
-from data_analysis.interactions_analysis.find_intreactions import calculate_interactions
-from data_analysis.interactions_analysis.print_interactions_results import print_output
-from data_analysis.interactions_analysis.plot_interactions import create_scatter_plot,visualization_protein
-from data_analysis.best_model.output_best_model import get_best_model
 
-import glob
+import streamlit as st
+import json
+from data_analysis.json_process.utils.extrect_data import extract_data_from_full_data_json
+
+from data_analysis.best_model.output_best_model import get_best_model
+from draws.tabs import tab1_data_protein,tab2_raw_data,tab3_plots,tab4_visual,tab5_inerpert
 
     
 def draw_output_data(folder_path):
@@ -56,37 +51,8 @@ def draw_output_data(folder_path):
     
     
     tab1, tab2,tab3,tab4,tab5 = st.tabs([ "🗃 Data of the protein"," 📝 raw data","📈plots ","💊 visualization","📖output interpret"])
-    with tab1:
-        summery_pattern = f"_summary_confidences_{sel_model_num}.json"
-        job_summery_files = [file for file in files if file.endswith(summery_pattern)]
-
-        print_output(tab1,sequences,sequences_len, proteins_names, atom_plddts,job_summery_files[0],folder_path)
-
-        
-    pae_threshold = tab2.slider('Select PAE Threshold', min_value=0.0, max_value=max(full_pae.flatten()), value=max(full_pae.flatten())/2, step=0.1)
-    contact_prob_threshold = tab2.slider('Select Contact Probability Threshold', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-    inter_chain_interactions = calculate_interactions(sequences, full_pae, contact_probs, token_chain_ids,pae_threshold ,contact_prob_threshold)
-    df_interactions = pd.DataFrame(inter_chain_interactions,columns=['Chain_1','Residue_1','Position_1','Chain_2','Residue_2','Position_2','PAE','Probability']).sort_values(by='Probability', ascending=False)
-    tab2.subheader('Interactions between protein A to B')
-    tab2.info('Notice : **low PAE** and **high Probability** means high probability of connection . ', icon="ℹ️")
-    tab2.write(df_interactions)
-
-    heatmap_fig = create_scatter_plot(df_interactions)
-    tab3.plotly_chart(heatmap_fig)
-    
-
-    
-    with tab4:
-        pattern = f"*_model_{sel_model_num}.cif"
-        found_files = glob.glob(os.path.join(folder_path, pattern))
-        tab4.title('CIF File Viewer for Protein Models')
-        visualization_protein(found_files)
-    with tab5:
-        st.markdown(':petri_dish: \t :blue-background[pLDDT]: a per-atom confidence estimate on a 0-100 scale where a higher value indicates higher confidence.\n')
-        st.markdown(':petri_dish: \t :blue-background[PAE (predicted aligned error)]: estimate of the error in the relative position and orientation between two tokens in the predicted structure. Higher values indicate higher predicted error and therefore lower confidence\n')
-        st.markdown(':petri_dish: \t :blue-background[ptm]: A scalar in the range 0-1 indicating the predicted TM-score for the full structure.\n')
-        st.markdown(':petri_dish: \t :blue-background[iptm]: A scalar in the range 0-1 indicating predicted interface TM-score (confidence in the predicted interfaces) for all interfaces in the structure.\n')
-        st.markdown(':petri_dish: \t :blue-background[fraction_disordered]: A scalar in the range 0-1 that indicates what fraction of the prediction structure is disordered, as measured by accessible surface area.\n')
-        st.markdown(':petri_dish: \t :blue-background[has_clash]:  A boolean indicating if the structure has a significant number of clashing atoms (more than 50% of a chain, or a chain with more than 100 clashing atoms)..\n')
-        st.markdown(':petri_dish: \t :blue-background[ranking_score]: e: A scalar in the range [-100, 1.5] that can be used for ranking predictions.\n')
-        st.code('0.8 × ipTM + 0.2 × pTM + 0.5 × disorder − 100 × has_clash')
+    tab1_data_protein(folder_path, files, sel_model_num, sequences_len, sequences, proteins_names, atom_plddts, tab1)
+    df_interactions = tab2_raw_data(sequences, full_pae, contact_probs, token_chain_ids, tab2)
+    tab3_plots(tab3, df_interactions)
+    tab4_visual(folder_path, sel_model_num, tab4)   
+    tab5_inerpert(tab5)
---
+++ draws/tabs.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+from data_analysis.interactions_analysis.find_intreactions import calculate_interactions
+from data_analysis.interactions_analysis.print_interactions_results import print_output
+from data_analysis.interactions_analysis.plot_interactions import create_scatter_plot,visualization_protein
+
+def tab4_visual(folder_path, sel_model_num, tab4):
+    with tab4:
+        pattern = f"*_model_{sel_model_num}.cif"
+        found_files = glob.glob(os.path.join(folder_path, pattern))
+        tab4.title('CIF File Viewer for Protein Models')
+        visualization_protein(found_files)
+
+def tab3_plots(tab3, df_interactions):
+    heatmap_fig = create_scatter_plot(df_interactions)
+    tab3.plotly_chart(heatmap_fig)
+
+def tab2_raw_data(sequences, full_pae, contact_probs, token_chain_ids, tab2):
+    with tab2:
+        tab2.subheader('Interactions between protein A to B')
+        pae_threshold = tab2.slider('Select PAE Threshold', min_value=0.0, max_value=max(full_pae.flatten()), value=max(full_pae.flatten())/2, step=0.1)
+        contact_prob_threshold = tab2.slider('Select Contact Probability Threshold', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+        inter_chain_interactions = calculate_interactions(sequences, full_pae, contact_probs, token_chain_ids,pae_threshold ,contact_prob_threshold)
+        df_interactions = pd.DataFrame(inter_chain_interactions,columns=['Chain_1','Residue_1','Position_1','Chain_2','Residue_2','Position_2','PAE','Probability']).sort_values(by='Probability', ascending=False)
+        tab2.info('Notice : **low PAE** and **high Probability** means high probability of connection . ', icon="ℹ️")
+        tab2.write(df_interactions)
+    return df_interactions
+
+def tab1_data_protein(folder_path, files, sel_model_num, sequences_len, sequences, proteins_names, atom_plddts, tab1):
+    with tab1:
+        summery_pattern = f"_summary_confidences_{sel_model_num}.json"
+        job_summery_files = [file for file in files if file.endswith(summery_pattern)]
+        st.subheader("proteins details")
+        print_output(tab1,sequences,sequences_len, proteins_names, atom_plddts,job_summery_files[0],folder_path)
+
+
+
+def tab5_inerpert(tab5):
+    with tab5:
+        st.markdown(':petri_dish: \t :blue-background[pLDDT]: a per-atom confidence estimate on a 0-100 scale where a higher value indicates higher confidence.\n')
+        st.markdown(':petri_dish: \t :blue-background[PAE (predicted aligned error)]: estimate of the error in the relative position and orientation between two tokens in the predicted structure. Higher values indicate higher predicted error and therefore lower confidence\n')
+        st.markdown(':petri_dish: \t :blue-background[ptm]: A scalar in the range 0-1 indicating the predicted TM-score for the full structure.\n')
+        st.markdown(':petri_dish: \t :blue-background[iptm]: A scalar in the range 0-1 indicating predicted interface TM-score (confidence in the predicted interfaces) for all interfaces in the structure.\n')
+        st.markdown(':petri_dish: \t :blue-background[fraction_disordered]: A scalar in the range 0-1 that indicates what fraction of the prediction structure is disordered, as measured by accessible surface area.\n')
+        st.markdown(':petri_dish: \t :blue-background[has_clash]:  A boolean indicating if the structure has a significant number of clashing atoms (more than 50% of a chain, or a chain with more than 100 clashing atoms)..\n')
+        st.markdown(':petri_dish: \t :blue-background[ranking_score]: e: A scalar in the range [-100, 1.5] that can be used for ranking predictions.\n')
+        st.code('0.8 × ipTM + 0.2 × pTM + 0.5 × disorder − 100 × has_clash')
+
